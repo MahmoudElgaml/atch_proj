1ad9631b735a9852ae6ac1cf121ab7af818f9894
Mahmoud <mahmoudhussien743@gmail.com>
2024-08-01 12:28:30 +0300

    add r to advertiser

--- lib/feature/auth_feature/auth/presentation/widgets/custom_drop_menu.dart
+++ lib/feature/auth_feature/auth/presentation/widgets/custom_drop_menu.dart
@@ -11,7 +11,7 @@ class CustomDropMenu extends StatefulWidget {
 class _CustomDropMenuState extends State<CustomDropMenu> {
   final List<String> items = [
     'user',
-    'advertise',
+    'advertiser',
   ];
 
   String selectedValue = "user";
