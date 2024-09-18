7e44d7d47f6acd26c3e06a8b867ffd6aa732b515
Elgaml <mahmoudelgmal991@gmail.com>
2024-09-19 00:01:33 +0300

    - add new Icon for contact_us_dialog - add lunch link functionality to contact_us_dialog - add lunch whats to helper class - create enum for the data

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -19,11 +19,13 @@ class Assets {
   static const String imagesGLogo = 'assets/images/g_logo.png';
   static const String imagesGoogle = 'assets/images/google.png';
   static const String imagesImageTest = 'assets/images/image_test.png';
+  static const String imagesInstagramIcon = 'assets/images/instagram_icon.svg';
   static const String imagesJazz = 'assets/images/jazz.png';
   static const String imagesLinkedinIcon = 'assets/images/linkedin_icon.svg';
   static const String imagesListIcon = 'assets/images/list_icon.png';
   static const String imagesLocation = 'assets/images/Location.png';
   static const String imagesLogInPhoto = 'assets/images/log_in_photo.png';
+  static const String imagesMailIcon = 'assets/images/mail_icon.svg';
   static const String imagesMiniLogo = 'assets/images/mini_logo.png';
   static const String imagesNotification = 'assets/images/notification.png';
   static const String imagesProfileTestImage = 'assets/images/profile_test_image.png';
