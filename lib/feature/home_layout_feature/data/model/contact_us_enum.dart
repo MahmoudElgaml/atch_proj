53524e4675b8096a323ccfe49db8f1aa6d1fdadf
Elgaml <mahmoudelgmal991@gmail.com>
2024-11-18 08:45:51 +0200

    - add new instagram photo - finish new contact us dialog

--- lib/feature/home_layout_feature/data/model/contact_us_enum.dart
+++ lib/feature/home_layout_feature/data/model/contact_us_enum.dart
@@ -5,8 +5,6 @@ enum ContactUsModel {
       "https://www.facebook.com/profile.php?id=61550837405127&mibextid=LQQJ4d"),
   instagram(Assets.imagesInstagramIcon,
       "https://www.instagram.com/greateagleeg?igsh=a3NwYjNmYmc2b281"),
-  linkedIn(Assets.imagesLinkedinIcon,
-      "https://www.linkedin.com/in/great-eagle-428375271/"),
   whats(Assets.imagesWhatsIcon, "01555504487");
 
   final String icon;
